4f3ea4e5de615f1a74e3e436a89894cbcb54cf7d
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-06 14:57:27 +0100

    Created DELETE by ID method. Little fixes in GETs methods.

--- sprint-1/week-1/src/index.ts
+++ sprint-1/week-1/src/index.ts
@@ -9,26 +9,35 @@ const app = express();
 /*Return all videos*/
 app.get("/hometask_01/api/videos",(request:Request, response:Response):void => {
     if(videos.length > 0){
-        response.send(videos).statusCode= 200;
+        response.status(200).send(videos);
     }
-    else{response.statusCode = 404;}
+    else{response.status(404);}
 })
-
-app.put("/hometask_01/api/videos",(request:Request, response:Response):void=> {
-
-})
-
 /*Get by ID*/
 app.get(`/hometask_02/api/videos/:id`,(request:Request, response:Response):void => {
     let specificVideo = videos.find(item=> item.id === +request.params.id);
     if(specificVideo){
-        response.send(specificVideo).statusCode = 200;
+        response.status(200).send(specificVideo);
     }
     else{
-        response.statusCode = 404;
-    }
+        response.status(404);}
+})
+app.put("/hometask_01/api/videos",(request:Request, response:Response):void=> {
 
 })
+
+/*Delete by ID*/
+app.delete(`/hometask_02/api/videos/:id`,(request:Request, response:Response):void => {
+    for(let i:number=0; i<videos.length; i++){
+        if(videos[i].id === +request.params.id){
+            videos.splice(i,1);
+            response.status(204);
+            return;
+        }
+        response.status(404);
+    }
+})
+
 
 app.listen(SETTINGS.PORT, () => {
     console.log('...server started in port ' + SETTINGS.PORT)
